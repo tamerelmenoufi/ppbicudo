7e968373f389f8df47b0fcdf2bf547b0302d034f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-21 09:29:24 +0200

    Atualizações

--- src/planilhas/index.php
+++ src/planilhas/index.php
@@ -14,34 +14,28 @@
 
     if($_POST['situacao']){
 
-      // Caminho para o arquivo XLSX que você deseja ler
       $xlsxFilePath = '../../planilhas/modelo.xlsx';
-
-      // Carrega o arquivo XLSX
       $spreadsheet = IOFactory::load($xlsxFilePath);
-
-      // Obtém a primeira planilha do arquivo
       $worksheet = $spreadsheet->getActiveSheet();
-
-      // Obtém o número total de linhas e colunas
-      $highestRow = $worksheet->getHighestRow(); // Número total de linhas
-      $highestColumn = $worksheet->getHighestColumn(); // Última coluna
-
-      // Itera sobre as células
-      echo "<table border = '1'>";
-
+      $highestRow = $worksheet->getHighestRow();
+      $highestColumn = $worksheet->getHighestColumn();
+      $campos = [];
+      $retorno = [];
       for ($row = 1; $row <= $highestRow; $row++) {
-          echo "<tr>";
           for ($col = 'A'; $col <= $highestColumn; $col++) {
               $cellValue = $worksheet->getCell($col . $row)->getValue();
               // Faça algo com o valor da célula, por exemplo, exiba-o
               // echo "Valor na célula {$col}{$row}: " . $cellValue . "<br>";
-              echo "<td>" . $cellValue . "</td>";
+              if($row == 1){
+                $campos[$col] = $cellValue;
+              }else{
+                $retorno[][$campos[$col]] = $cellValue;
+              }
           }
-          echo "</tr>";
       }
-      echo "</table>";
 
+      echo json_encode($retorno);
+      
       // $query = "update planilhas set situacao = '{$_POST['opc']}' where codigo = '{$_POST['situacao']}'";
       // mysqli_query($con, $query);
       exit();
