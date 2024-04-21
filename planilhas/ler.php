0a3ea3beab0dbb28ee85109c640a2151127478ba
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-21 18:39:45 +0200

    Atualizações

--- planilhas/ler.php
+++ planilhas/ler.php
@@ -1,36 +1,35 @@
 <?php
 
-// Inclua o autoload.php do PhpSpreadsheet
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; // Caminho para o autoload gerado pelo Composer
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-try {
-    // Carregue o arquivo Excel
-    $spreadsheet = IOFactory::load('modelo.xlsx');
-    
-    // Obtenha a primeira planilha no arquivo
-    $sheet = $spreadsheet->getActiveSheet();
-    
-    // Obtenha o número de linhas e colunas na planilha
-    $highestRow = $sheet->getHighestRow();
-    $highestColumn = $sheet->getHighestColumn();
-    
-    // Converta a letra da coluna para o número correspondente
-    $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn);
-    
-    // Loop pelas células da planilha
-    for ($row = 1; $row <= $highestRow; $row++) {
-        for ($col = 1; $col <= $highestColumnIndex; $col++) {
-            // Obtenha o valor da célula
-            $value = $sheet->getCellByColumnAndRow($col, $row)->getValue();
-            // Imprima o valor da célula
-            echo "Valor da célula $col-$row: $value\n";
-        }
-    }
-} catch (\PhpOffice\PhpSpreadsheet\Reader\Exception $e) {
-    // Lidar com exceções de leitura de arquivo
-    echo 'Exceção ao ler o arquivo: ', $e->getMessage();
-}
+// Caminho para o arquivo XLSX que você deseja ler
+// $xlsxFilePath = $_POST['arquivo'];
+$xlsxFilePath = "modelo.xlsx";
 
+// Carrega o arquivo XLSX
+$spreadsheet = IOFactory::load($xlsxFilePath);
+
+// Obtém a primeira planilha do arquivo
+$worksheet = $spreadsheet->getActiveSheet();
+
+// Obtém o número total de linhas e colunas
+$highestRow = $worksheet->getHighestRow(); // Número total de linhas
+$highestColumn = $worksheet->getHighestColumn(); // Última coluna
+
+// Itera sobre as células
+echo "<table border = '1'>";
+
+for ($row = 1; $row <= $highestRow; $row++) {
+    echo "<tr>";
+    for ($col = 'A'; $col <= $highestColumn; $col++) {
+        $cellValue = $worksheet->getCell($col . $row)->getValue();
+        // Faça algo com o valor da célula, por exemplo, exiba-o
+        // echo "Valor na célula {$col}{$row}: " . $cellValue . "<br>";
+        echo "<td>" . $cellValue . "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
 ?>
