70a3af35150f574704a879ce55112a672e5c2c39
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-21 22:28:06 +0200

    Atualizações

--- src/origens/index.php
+++ src/origens/index.php
@@ -121,7 +121,7 @@
                     <div class="row">
                       <div class="col-12 d-flex justify-content-end">
                         <div class="form-check form-switch">
-                          <input class="form-check-input situacao" type="checkbox" <?=(($d->codigo == 1)?'disabled':false)?> <?=(($d->status)?'checked':false)?> situacao="<?=$d->codigo?>">
+                          <input class="form-check-input situacao" type="checkbox" <?=(($d->status)?'checked':false)?> situacao="<?=$d->codigo?>">
                           Situação
                         </div>
                       </div>
