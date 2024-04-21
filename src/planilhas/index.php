bd73c43a1ba11f4b526969c93eee60301e6b8ce5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-21 09:12:06 +0200

    Atualizações

--- src/planilhas/index.php
+++ src/planilhas/index.php
@@ -58,7 +58,7 @@
               </thead>
               <tbody>
                 <?php
-                  $query = "select a.*, b.nome as usuario_nome from planilhas a lefy join usuarios b on a.usuario = b.codigo order by a.data desc";
+                  $query = "select a.*, b.nome as usuario_nome from planilhas a left join usuarios b on a.usuario = b.codigo order by a.data desc";
                   $result = mysqli_query($con, $query);
                   while($d = mysqli_fetch_object($result)){
                 ?>
