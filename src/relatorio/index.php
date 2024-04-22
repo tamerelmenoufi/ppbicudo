6c7bab0c320cb9c221ea13e7a7f34407ad638464
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-22 23:49:14 +0200

    Atualizações

--- src/relatorio/index.php
+++ src/relatorio/index.php
@@ -24,7 +24,7 @@
 
     if($_SESSION['usuarioBusca']){
       // $cpf = str_replace( '.', '', str_replace('-', '', $_SESSION['usuarioBusca']));
-      // $where = " and nome like '%{$_SESSION['usuarioBusca']}%' or REPLACE( REPLACE( cpf, '.', '' ), '-', '' ) = '{$cpf}' ";
+      $where = " and origem = '{$_SESSION['buscaOrigem']}' and data between '{$_SESSION['buscaDataInicial']}' and '{$_SESSION['buscaDataFinal']}' ";
     }
 
 
