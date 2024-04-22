ddac88124b91529f389f946041aeaffc2223d907
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-22 02:09:28 +0200

    Atualizações

--- print.php
+++ print.php
@@ -17,51 +17,45 @@
 
 
 
-    <table class="table table-striped">
+    <table class="table table-striped table-hover">
         <thead>
-        <tr>
-            <th scope="col">Lote</th>
-            <th scope="col">Origem</th>
+            <tr>
             <th scope="col">Data</th>
-            <th scope="col">Usuário</th>
-            <th scope="col">Situação</th>
-            <th scope="col" width="80">Ações</th>
-        </tr>
+            <th scope="col">Anúncios</th>
+            <th scope="col">Pagamento Produto</th>
+            <th scope="col">Pagamento Frete</th>
+            <th scope="col">Custo Produto</th>
+            <th scope="col">Custo Frete</th>
+            <th scope="col">Comissão</th> 
+            <th scope="col">Lucro</th>
+            <th scope="col">Frete</th>
+            <th scope="col">Porcentagem</th>
+            <th scope="col">Código do Produto</th>
+            </tr>
         </thead>
         <tbody>
-        <?php
-            $query = "select a.*, b.nome as usuario_nome, o.nome as origem, (select count(*) from relatorio where planilha = a.codigo) as registros from planilhas a left join usuarios b on a.usuario = b.codigo left join origens o on a.origem = o.codigo order by a.data desc limit 100";
-            $result = mysqli_query($con, $query);
+            <?php
+            $query = "select * from relatorio where 1 {$where} order by dataCriacao desc";
+            $result = mysqli_query($con,$query);
+            
             while($d = mysqli_fetch_object($result)){
-        ?>
-        <tr>
-            <td style="white-space: nowrap;"><?=strtoupper($d->lote)?></td>
-            <td style="white-space: nowrap;"><?=$d->origem?></td>
-            <td style="white-space: nowrap;"><?=dataBr($d->data)?></td>
-            <td style="white-space: nowrap;"><?=$d->usuario_nome?></td>
-            <td style="white-space: nowrap;">
-            <i 
-                situacao="<?=$d->codigo?>" 
-                planilha="<?=$d->planilha?>" 
-                class="fa-solid fa-file-arrow-up text-<?=(($d->situacao == '1' and $d->registros)?'success':'secondary situacao')?>" 
-                style="font-size:30px; <?=(($d->situacao == '1' and $d->registros)?false:'cursor:pointer')?>"
-            ></i> <?=(($d->registros)?:false)?>
-            </td>
-            <td acoes style="white-space: nowrap;">
-            <?php
-            if(!$d->registros){
             ?>
-            <button class="btn btn-danger btn-sm" deletar="<?=$d->codigo?>" planilha="<?=$d->planilha?>">
-            <i class="fa-solid fa-trash-can"></i> Excluir
-            </button>
+            <tr>
+            <td class="text-nowrap"><?=dataBr($d->dataCriacao)?></td>
+            <td class=""><?=$d->tituloItem?></td>
+            <td class="text-nowrap">R$<?=number_format($d->ValorPedidoXquantidade,2,',','.')?></td>
+            <td class="text-nowrap">R$<?=number_format($d->CustoEnvio,2,',','.')?></td>
+            <td class="text-nowrap">R$<?=number_format($d->PrecoCusto,2,',','.')?></td>
+            <td class="text-nowrap">R$<?=number_format($d->CustoEnvioSeller,2,',','.')?></td>
+            <td class="text-nowrap">R$<?=number_format(($d->TarifaGatwayPagamento + $d->TarifaMarketplace),2,',','.')?></td>
+            <td class="text-nowrap">R$<?=number_format(($d->ValorPedidoXquantidade - $d->PrecoCusto - $d->CustoEnvioSeller - $d->TarifaGatwayPagamento - $d->TarifaMarketplace),2,',','.')?></td>
+            <td class="text-nowrap"><?=$d->frete?></td>
+            <td class="text-nowrap"><?=number_format($d->Porcentagem,2,',','.')?>%</td>
+            <td class="text-nowrap"><?=$d->codigoPedido?></td>
+            </tr>
             <?php
             }
             ?>
-            </td>
-        </tr>
-        <?php
-            }
-        ?>
         </tbody>
     </table>
 
