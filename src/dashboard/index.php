048aa839e73570575883372b92939ac934e0ffe3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-23 08:21:13 +0200

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -15,12 +15,26 @@
 </style>
 <div class="m-3">
     <div class="row g-0">
-        <div class="col-md-12">
-            DashBoard
+        <div class="col-md-4">
+            <div class="alert alert-secondary" role="alert">
+                <span>Planilhas Importadas</span>
+                <h1>136</h1>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="alert alert-primary" role="alert">
+                <span>Total de Vendas</span>
+                <h1>2693</h1>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="alert alert-success" role="alert">
+                <span>Total Arrecadado</span>
+                <h1>R$ 126.851,97</h1>
+            </div>
         </div>
     </div>
 </div>
-
 
 
 <script>
