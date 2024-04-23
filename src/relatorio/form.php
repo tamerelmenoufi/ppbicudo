9cf0a06e74f020d5f38e8c0045126e4cfb7d0309
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-23 12:30:37 +0200

    Atualizações

--- src/relatorio/form.php
+++ src/relatorio/form.php
@@ -37,6 +37,9 @@
     top:7px;
     margin-left:50px;
   }
+  .deletado{
+    display:<?=(($d->deletado)?'block':'none')?>
+  }
 </style>
 
 <h3 class="titulo<?=$md5?>">Editar Registro de Venda</h3>
@@ -44,70 +47,81 @@
     <form id="acaoMenu">
 
         <div class="form-floating mb-3">
-            <input type="text" name="dataCriacao" id="dataCriacao" class="form-control" placeholder="dataCriacao" value="<?=$d->dataCriacao?>">
+            <input type="text" require name="dataCriacao" id="dataCriacao" class="form-control" placeholder="dataCriacao" value="<?=$d->dataCriacao?>">
             <label for="dataCriacao">dataCriacao*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="codigoPedido" id="codigoPedido" class="form-control" placeholder="codigoPedido" value="<?=$d->codigoPedido?>">
+            <input type="text" require name="codigoPedido" id="codigoPedido" class="form-control" placeholder="codigoPedido" value="<?=$d->codigoPedido?>">
             <label for="codigoPedido">codigoPedido*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="pedidoOrigem" id="pedidoOrigem" class="form-control" placeholder="pedidoOrigem" value="<?=$d->pedidoOrigem?>">
+            <input type="text" require name="pedidoOrigem" id="pedidoOrigem" class="form-control" placeholder="pedidoOrigem" value="<?=$d->pedidoOrigem?>">
             <label for="pedidoOrigem">pedidoOrigem*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="tituloItem" id="tituloItem" class="form-control" placeholder="tituloItem" value="<?=$d->tituloItem?>">
+            <input type="text" require name="tituloItem" id="tituloItem" class="form-control" placeholder="tituloItem" value="<?=$d->tituloItem?>">
             <label for="tituloItem">tituloItem*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="frete" id="frete" class="form-control" placeholder="frete" value="<?=$d->frete?>">
+            <input type="text" require name="frete" id="frete" class="form-control" placeholder="frete" value="<?=$d->frete?>">
             <label for="frete">frete*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="ValorPedidoXquantidade" id="ValorPedidoXquantidade" class="form-control" placeholder="ValorPedidoXquantidade" value="<?=$d->ValorPedidoXquantidade?>">
+            <input type="text" require name="ValorPedidoXquantidade" id="ValorPedidoXquantidade" class="form-control" placeholder="ValorPedidoXquantidade" value="<?=$d->ValorPedidoXquantidade?>">
             <label for="ValorPedidoXquantidade">ValorPedidoXquantidade*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="CustoEnvio" id="CustoEnvio" class="form-control" placeholder="CustoEnvio" value="<?=$d->CustoEnvio?>">
+            <input type="text" require name="CustoEnvio" id="CustoEnvio" class="form-control" placeholder="CustoEnvio" value="<?=$d->CustoEnvio?>">
             <label for="CustoEnvio">CustoEnvio*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="CustoEnvioSeller" id="CustoEnvioSeller" class="form-control" placeholder="CustoEnvioSeller" value="<?=$d->CustoEnvioSeller?>">
+            <input type="text" require name="CustoEnvioSeller" id="CustoEnvioSeller" class="form-control" placeholder="CustoEnvioSeller" value="<?=$d->CustoEnvioSeller?>">
             <label for="CustoEnvioSeller">CustoEnvioSeller*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="TarifaGatwayPagamento" id="TarifaGatwayPagamento" class="form-control" placeholder="TarifaGatwayPagamento" value="<?=$d->TarifaGatwayPagamento?>">
+            <input type="text" require name="TarifaGatwayPagamento" id="TarifaGatwayPagamento" class="form-control" placeholder="TarifaGatwayPagamento" value="<?=$d->TarifaGatwayPagamento?>">
             <label for="TarifaGatwayPagamento">TarifaGatwayPagamento*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="TarifaMarketplace" id="TarifaMarketplace" class="form-control" placeholder="TarifaMarketplace" value="<?=$d->TarifaMarketplace?>">
+            <input type="text" require name="TarifaMarketplace" id="TarifaMarketplace" class="form-control" placeholder="TarifaMarketplace" value="<?=$d->TarifaMarketplace?>">
             <label for="TarifaMarketplace">TarifaMarketplace*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="PrecoCusto" id="PrecoCusto" class="form-control" placeholder="PrecoCusto" value="<?=$d->PrecoCusto?>">
+            <input type="text" require name="PrecoCusto" id="PrecoCusto" class="form-control" placeholder="PrecoCusto" value="<?=$d->PrecoCusto?>">
             <label for="PrecoCusto">PrecoCusto*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="Porcentagem" id="Porcentagem" class="form-control" placeholder="Porcentagem" value="<?=$d->Porcentagem?>">
+            <input type="text" require name="Porcentagem" id="Porcentagem" class="form-control" placeholder="Porcentagem" value="<?=$d->Porcentagem?>">
             <label for="Porcentagem">Porcentagem*</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="Conta" id="Conta" class="form-control" placeholder="Conta" value="<?=$d->Conta?>">
+            <input type="text" require name="Conta" id="Conta" class="form-control" placeholder="Conta" value="<?=$d->Conta?>">
             <label for="Conta">Conta*</label>
         </div>
 
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="deletado" <?=(($d->deletado)?'checked':false)?>>
+          <label class="form-check-label" for="deletado">Remover Registro da planilha</label>
+        </div>
+
+        <div class="deletado">
+        <div class="form-floating mb-3">
+          <textarea name="deletado_justificativa" id="deletado_justificativa" class="form-control" rows="10"><?=$d->deletado_justificativa?></textarea>
+            <label for="Conta">Justificativa*</label>
+        </div>
+        </div>
 
         <button cancelar type="button" data-bs-dismiss="offcanvas" class="btn btn-danger mt-3"> <i class="fa fa-cancel"></i> Cancelar</button>
         <button type="submit" data-bs-dismiss="offcanvas" class="btn btn-primary mt-3"> <i class="fa fa-save"></i> Importar Planilha</button>
@@ -123,6 +137,15 @@
     $(function(){
 
       Carregando('none');
+
+      $("#deletado").click(function(){
+        if($(this).prop("checked") == true){
+          $(".deletado").css("display","block");
+        }else{
+          $(".deletado").css("display","none");
+        }
+        $("#deletado_justificativa").html('');
+      })
 
       $( "form" ).on( "submit", function( event ) {
         Carregando();
