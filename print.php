863078710d5a8c3e04992bf2f62451490a883021
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-23 07:32:12 +0200

    Atualizações

--- print.php
+++ print.php
@@ -62,9 +62,9 @@
             <tr>
               <th colspan = "11" >
                 <div class="row">
-                  <div class="col"></div>
-                  <div class="col"><img src="img/logo.png?1" height="60" alt=""></div>
-                  <div class="col"></div>
+                  <div class="col justify-content-center"></div>
+                  <div class="col justify-content-center"><img src="img/logo.png?1" height="60" alt=""></div>
+                  <div class="col justify-content-center"></div>
                 </div>
               </th>
             </tr>
