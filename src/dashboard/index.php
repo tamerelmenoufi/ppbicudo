782f19b724631ca2f3e833fb98503c014ebeb154
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-23 11:34:06 +0200

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -16,7 +16,9 @@
                 (select sum(PrecoCusto) from relatorio ) as custo_produto,   
                 (select sum(CustoEnvioSeller) from relatorio ) as custo_frete,   
                 (select sum(TarifaGatwayPagamento + TarifaMarketplace) from relatorio ) as comissão,   
-                (select sum(ValorPedidoXquantidade - PrecoCusto - CustoEnvioSeller - TarifaGatwayPagamento - TarifaMarketplace) from relatorio) as lucro   
+                (select sum(ValorPedidoXquantidade - PrecoCusto - CustoEnvioSeller - TarifaGatwayPagamento - TarifaMarketplace) from relatorio) as lucro,
+                (select count(*) from planilhas) as planilhas,
+                (select count(*) from relatorio) as vendas
         ";
     $r = mysqli_query($con, $q);
     $v = mysqli_fetch_object($r);
@@ -37,19 +39,19 @@
         <div class="col-md-4 p-2">
             <div class="alert alert-secondary" role="alert">
                 <span>Planilhas Importadas</span>
-                <h1>136</h1>
+                <h1><?=$v->planilhas?></h1>
             </div>
         </div>
         <div class="col-md-4 p-2">
             <div class="alert alert-primary" role="alert">
                 <span>Total de Vendas</span>
-                <h1>2693</h1>
+                <h1><?=$v->vendas?></h1>
             </div>
         </div>
         <div class="col-md-4 p-2">
             <div class="alert alert-success" role="alert">
                 <span>Total Arrecadado</span>
-                <h1>R$ 126.851,97</h1>
+                <h1>R$ <?=number_format($v->pagamento_produto,2,',','.')?></h1>
             </div>
         </div>
     </div>
