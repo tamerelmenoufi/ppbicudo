c6e688c77bd9df425fa8ea3e0bccbff1f307e0f8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-23 08:42:34 +0200

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -92,7 +92,7 @@
         <div class="col-md-1 p-2">
             <div class="alert alert-secondary" role="alert">
                 <span style="color:#a1a1a1; font-size:12px;">Mês <?=$i+1?></span>
-                <div style="font-size:14; font-weight:bold">R$ 154.999,64</div>
+                <div style="font-size:13; font-weight:bold">R$ 154.999,64</div>
             </div>
         </div>
         <?php
