b1012714e57f4b1f50a5a18bdfa105ff06f47345
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-26 01:52:42 +0300

    Atualizações

--- src/relatorio/relatorios.php
+++ src/relatorio/relatorios.php
@@ -1,6 +1,11 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
 ?>
+<style>
+    span[edit]{
+        cursor:pointer;
+    }
+</style>
 <ul class="list-group">
 <?php
     $query = "select * from relatorio_modelos order by data desc";
@@ -9,7 +14,7 @@
 ?>
   <li class="list-group-item">
     <div class="d-flex justify-content-between">
-        <span><i class="fa-regular fa-pen-to-square"></i> <?=$d->nome?></span>
+        <span edit="<?=$d->codigo?>"><i class="fa-regular fa-pen-to-square"></i> <?=$d->nome?></span>
         <i class="fa-regular fa-trash-can"></i>
     </div>
   </li>
@@ -17,3 +22,24 @@
     }
 ?>
 </ul>
+
+<script>
+    $(function(){
+        $("span[edit]").click(function(){
+            modelo = $(this).attr("edit");
+            $.ajax({
+              url:"src/relatorio/index.php",
+              type:"POST",
+              data:{
+                modelo
+              },
+              success:function(dados){
+                $("#paginaHome").html(dados);
+                let myOffCanvas = document.getElementById('offcanvasDireita');
+                let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
+                openedCanvas.hide();
+              }
+          })
+        })
+    })
+</script>
