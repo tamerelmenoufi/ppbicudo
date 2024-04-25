1824e919d684dcda58152a437fe0dea1b0f3adc5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-25 20:26:58 +0200

    Atualizações

--- src/relatorio/form.php
+++ src/relatorio/form.php
@@ -80,8 +80,23 @@
         </div>
 
         <div class="form-floating mb-3">
+            <input type="text" require name="PrecoCusto" id="PrecoCusto" class="form-control" placeholder="Custo Produto" value="<?=$d->PrecoCusto?>">
+            <label for="PrecoCusto">Custo Produto*</label>
+        </div>
+
+        <div class="form-floating mb-3">
             <input type="text" require name="CustoEnvioSeller" id="CustoEnvioSeller" class="form-control" placeholder="Custo Frete" value="<?=$d->CustoEnvioSeller?>">
             <label for="CustoEnvioSeller">Custo Frete*</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" require name="TarifaGatwayPagamento" id="TarifaGatwayPagamento" class="form-control" placeholder="Taxa Entrega" value="<?=$d->TarifaGatwayPagamento?>">
+            <label for="TarifaGatwayPagamento">Taxa Entrega*</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" require name="TarifaMarketplace" id="TarifaMarketplace" class="form-control" placeholder="Taxa Marketplace" value="<?=$d->TarifaMarketplace?>">
+            <label for="TarifaMarketplace">Taxa Marketplace*</label>
         </div>
 
         <div class="form-floating mb-3">
