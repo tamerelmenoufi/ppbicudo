77a3d4e96301b74a1e59c360ba0945f13d9aa1bb
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-30 15:07:34 +0300

    Atualizações

--- src/relatorio/index.php
+++ src/relatorio/index.php
@@ -396,7 +396,7 @@ R$ <?=number_format($d['valor'],2,',',false)?>
 </div>
 
 
-<div class="d-flex justify-contents-end" style="position:fixed; bottom:0; left:0; right:0; height:90px; border:solid 1px red; padding-left:30px; padding-right:30px; background-color:#fff;">
+<div style="display: flex; justify-content: flex-end; position:fixed; bottom:0; left:0; right:0; height:90px; border:solid 1px red; padding-left:30px; padding-right:30px; background-color:#fff;">
 
 <table>
 <thead>
