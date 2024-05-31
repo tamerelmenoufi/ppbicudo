509ea972666a6c081e4ee210705d2d8dcf468df5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-06-01 02:32:36 +0300

    Atualizações

--- src/relatorio/novo.php
+++ src/relatorio/novo.php
@@ -114,7 +114,7 @@
         <input type="hidden" id="acao" name="acao" value="salvar" >
         <input type="hidden" id="relatorio" name="relatorio" value="<?=$_POST['relatorio']?>" >
         <input type="hidden" id="conta" name="conta" value="<?=$_POST['conta']?>" >
-        <input type="hidden" id="origem" name="origem" value="<?=$_POST['origem']?>" >
+        <input type="hidden" id="origem" name="origem" value="<?=(($_POST['origem'])?:'0')?>" >
         <input type="hidden" id="planilha" name="planilha" value="<?=$_POST['planilha']?>" >
 
     </form>
