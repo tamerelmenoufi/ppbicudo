b5135a73a83687e8e7c0b1b80cf1e90e03950df7
tamerelmenoufi <tamer@mohatron.com.br>
2024-12-16 15:44:22 -0400

    Atualizaçeõs

--- src/relatorio/index.php
+++ src/relatorio/index.php
@@ -1,4 +1,8 @@
 <?php
+    // Exibe todos os erros e avisos
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
 
     // devolucao,
