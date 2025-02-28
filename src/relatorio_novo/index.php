0ffb7a1abb665aad83182612ea7892936060978d
tamerelmenoufi <tamer@mohatron.com.br>
2025-02-28 08:55:24 -0400

    Atualizaçeõs

--- src/relatorio_novo/index.php
+++ src/relatorio_novo/index.php
@@ -694,7 +694,7 @@ R$ <?=number_format($d['valor'],2,',',false)?>
 <?php
   if($where){
 ?>
-<div style="display: flex; justify-content: flex-end; position:fixed; bottom:0; left:0; right:0; height:65px; border:solid 0px red; padding-top:10px; padding-bottom:10px; padding-left:30px; padding-right:100px; background-color:#fff; z-index:100;">
+<div style="display: flex; justify-content: flex-end; position:fixed; bottom:0; left:0; right:0; height:/*65*/90px; border:solid 0px red; padding-top:10px; padding-bottom:10px; padding-left:30px; padding-right:100px; background-color:#fff; z-index:100;">
 
 <table>
 <thead>
