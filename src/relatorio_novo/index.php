d58d0a2e61915baeda8faa7a6972ce1f134a70af
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-03-24 06:41:13 +0200

    Atualizações

--- src/relatorio_novo/index.php
+++ src/relatorio_novo/index.php
@@ -205,7 +205,7 @@ R$ <?=number_format($d['valor'],2,',',false)?>
     }
 
     if($modelo_campo == 'devolucoes'){
-      $where = " and a.devolucao = '1' and a.origem = '{$rel->origem}' and a.devolucao_data like '".substr($rel->data,0, -3)."%'";
+      $where = " and a.devolucao = '1' and a.devolucao_relatorio = '{$rel->codigo}' and a.origem = '{$rel->origem}' and a.devolucao_data like '".substr($rel->data,0, -3)."%'";
     }else if($modelo_campo == 'registros'){
       $where = $where_registros; 
     }
