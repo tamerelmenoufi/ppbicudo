bdaa819bcd692730a8433de3ded6c82b9467dddd
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-06-12 18:20:25 +0300

    first commit

--- lib/header.php
+++ lib/header.php
@@ -2,9 +2,6 @@
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet"
 >
-<script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&callback=initMap&v=weekly&language=pt&region=BR"
-></script>
 
 <link href="<?=$urlPainel?>lib/vendor/bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <link href="<?=$urlPainel?>lib/vendor/jquery-confirm-v3.3.4/dist/jquery-confirm.min.css" rel="stylesheet" >
