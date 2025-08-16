e498da4a5cce09cd7a195172e617ba2773ae3bb0
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-08-17 00:23:39 +0300

    first commit

--- src/componentes/menu_lateral/menu.php
+++ src/componentes/menu_lateral/menu.php
@@ -21,19 +21,19 @@
         </a>
       </div>
     </div>
-
+<!-- 
     <div class="row mb-1">
       <div class="col">
         <a url="src/relatorio/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-chart-line col-1"></i> <span class="col-11">Consultas</span>
         </a>
       </div>
-    </div>
+    </div> -->
 
     <div class="row mb-1">
       <div class="col">
         <a url="src/relatorio_novo/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
-          <i class="fa-solid fa-chart-line col-1"></i> <span class="col-11">Consultas <span class="text-danger" style="font-size:10px;"> (Novo)</span></span>
+          <i class="fa-solid fa-chart-line col-1"></i> <span class="col-11">Consultas</span>
         </a>
       </div>
     </div>
