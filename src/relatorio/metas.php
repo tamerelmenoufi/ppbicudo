e0b73e972a9a25297eaed62286e8959d34350370
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-09-15 21:30:20 +0300

    first commit

--- src/relatorio/metas.php
+++ src/relatorio/metas.php
@@ -7,13 +7,26 @@
 
 </style>
 <div class="m-3">
-    <h4>Relatório de Metas</h4>
+    <h4 atualiza>Relatório de Metas</h4>
 
 </div>
 <script>
     $(function(){
         Carregando('none');
         
+        $("h4[atualiza]").click(function(){
+            Carregando();
+            $.ajax({
+                url:"src/relatorio/metas.php",
+                success:function(dados){
+                    $("#paginaHome").html(dados);
+                },
+                error:function(){
+                    Carregando('none');
+                    alert('Erro')
+                }
+            });
+        })
 
     })
 </script>
