df4aaf94e6cf0ea33540674992ee60c2f25b9674
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-09-15 21:27:09 +0300

    first commit

---
+++ src/relatorio/metas.php
@@ -0,0 +1,19 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/lib/includes.php");
+
+
+?>
+<style>
+
+</style>
+<div class="m-3">
+    <h4>Relatório de Metas</h4>
+
+</div>
+<script>
+    $(function(){
+        Carregando('none');
+        
+
+    })
+</script>
