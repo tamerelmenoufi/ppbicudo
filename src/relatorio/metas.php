8714fa257e159a09846c2fd055a7abb953e62571
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2025-09-16 18:50:38 +0300

    first commit

--- src/relatorio/metas.php
+++ src/relatorio/metas.php
@@ -9,9 +9,6 @@
 <div class="m-3">
     <h4 atualiza>Relatório de Metas</h4>
 
-
-
-
 <?php
 
 
@@ -34,85 +31,45 @@
         ];
     }
 
-
-
-    // Definir o mês e ano desejado
-    $mes = 9; // Setembro
+    $mes = 9;
     $ano = 2025;
 
-    // Número de dias do mês
     $diasNoMes = date("t", mktime(0, 0, 0, $mes, 1, $ano));
 
 ?>
-    <table class="table table-hover">
-        <thead>
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Lojas</th>
+                    <?php
+                    for ($dia = 1; $dia <= $diasNoMes; $dia++) {
+                    ?>
+                    <th><?=$dia?></th>
+                    <?php
+                    }
+                    ?>
+                </tr>
+            </thead>
+        <?php
+        // Linhas com os registros
+        foreach ($empresas as $i => $v) {
+        ?>
             <tr>
-                <th>Lojas</th>
-                <?php
-                for ($dia = 1; $dia <= $diasNoMes; $dia++) {
-                ?>
-                <th><?=$dia?></th>
-                <?php
-                }
-                ?>
+                <td><?=$v?></td>
+        <?php
+            for ($dia = 1; $dia <= $diasNoMes; $dia++) {
+        ?>
+                <td><?=$r[$i][$dia]['bruto']?></td>
+        <?php
+            }
+        ?>    
             </tr>
-        </thead>
-    <?php
-    // Linhas com os registros
-    foreach ($empresas as $i => $v) {
-    ?>
-        <tr>
-            <td><?=$v?></td>
-    <?php
-        for ($dia = 1; $dia <= $diasNoMes; $dia++) {
-    ?>
-            <td><?=$r[$i][$dia]['bruto']?></td>
-    <?php
+        <?php
         }
-    ?>    
-        </tr>
-    <?php
-    }
-    ?>
-    </table>
-
-<br><br><br><br><br>
-
-
-
-
-    <table class="table table-hover">
-        <tr>
-            <td>Loja</td>
-            <td>Quantidade</td>
-            <td>Valor Bruto</td>
-            <td>Valor Líquido</td>
-        </tr>
-    <?php
-    echo $query = "select 
-                    a.*,
-                    count(*) as qt,
-                    b.nome as origem_nome,
-                    day(a.dataCriacao) as dia,
-                    sum(a.ValorPedidoXquantidade) as bruto, 
-                    (sum(a.ValorPedidoXquantidade) - sum(a.PrecoCusto)) as lucro 
-                from relatorio a
-                    left join origens b on a.origem = b.codigo 
-                where date(a.dataCriacao) like '".date("Y-m")."%' group by day(a.dataCriacao), a.origem order by b.nome asc ";
-    $result = mysqli_query($con, $query);
-    while($d = mysqli_fetch_object($result)){
-    ?>
-        <tr>
-            <td><?=$d->origem_nome?></td>
-            <td><?=$d->qt?></td>
-            <td><?=$d->bruto?></td>
-            <td><?=$d->lucro?></td>
-        </tr>
-    <?php
-    }
-    ?>
-    </table>
-
+        ?>
+        </table>
+    </div>
 </div>
 <script>
     $(function(){
