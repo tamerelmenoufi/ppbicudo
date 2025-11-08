c9d365684d9fc99ec209109a56ab847db0d19f6e
tamerelmenoufi <tamer@mohatron.com.br>
2025-11-08 16:46:36 -0400

    Atualizações

--- src/relatorio/metas.php
+++ src/relatorio/metas.php
@@ -106,7 +106,7 @@
     $meta_p1 = number_format($meta_p1,2,',',false)."%";
     $meta_p2 = number_format($meta_p2,2,',',false)."%";
     $meta_p3 = number_format($meta_p3,2,',',false)."%";
-    //$lucro = number_format((($lucratividade/($vendas)?:1)*100),2,',',false)."%";
+    $lucro = number_format((($lucratividade/($vendas)?:1)*100),2,',',false)."%";
     //$vendas = "R$ ".number_format($vendas,2,',','.');
     //$lucratividade = "R$ ".number_format($lucratividade,2,',','.');
     
