4324a8f407387347b775d87d99608c2a89d96c7a
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2026-02-05 02:07:08 +0200

    Atualizacoes

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 
 // Prefer setting the credential via environment variable:
-putenv('PPBICUDO_API_CREDENCIAL=123456');
+// putenv('PPBICUDO_API_CREDENCIAL=123456');
 // If not set, you can hardcode below (not recommended for shared repos).
 
-define('PPBICUDO_API_CREDENCIAL', getenv('PPBICUDO_API_CREDENCIAL') ?: '');
+define('PPBICUDO_API_CREDENCIAL', 123456);
 
